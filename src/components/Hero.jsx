e7d2f16151177c9ca4b7b87fe639e7f354a3d164
NocturnJake <jarredexcel@gmail.com>
2026-06-26 23:42:40 +0800

    hero edits

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,28 @@
 import React from "react";
 import styles from "./Hero.module.css";
 import profpic from "../assets/profpic.jpg";
+
 function Hero() {
   return (
     <section id="hero" className={styles.heroContainer}>
       <div className={styles.content}>
         <h1 className={styles.title}>
-          Hi, I'm{" "}
-          <span className={styles.highlight}>Jarred Excel A. Oliveros</span>
+          Hi, I'm <span className={styles.highlight}>Jarred Excel A. Oliveros</span>
         </h1>
         <h2 className={styles.subtitle}>Web & Mobile App Developer</h2>
         <p className={styles.description}>
           A developer who consumes knowledge and continues to improve. Utilizing
-the different tools and platforms to discover new things and expand his
-current knowledge and experience. Specializes in creating projects that
-implement logical and functional features.
+          the different tools and platforms to discover new things and expand his
+          current knowledge and experience. Specializes in creating projects that
+          implement logical and functional features.
         </p>
         <a href="#projects" className={styles.heroBtn}>
           View My Work
         </a>
       </div>
       <div className={styles.imagePlaceholder}>
-        {/* Your profile image will go here */}
         <div className={styles.avatar}>
-          {" "}
-          <img src={profpic}></img>
+          <img src={profpic} alt="Jarred Oliveros Profile" />
         </div>
       </div>
     </section>
