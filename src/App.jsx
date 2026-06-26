55e97ca36accb5beaa1b089beebc24ee23677fce
NocturnJake <jarredexcel@gmail.com>
2026-06-26 23:33:29 +0800

    skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Certifications from "./components/Certifications";
 import Contact from "./components/Contact";
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Skills />
       <Projects />
       <Certifications />
       <Contact />
